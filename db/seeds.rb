feeca3d6f04a3a521f4956bafcadf333c0f0e19d
lekopien <mavericklekopien@gmail.com>
2023-03-06 10:56:41 +0300

    used a faker to add dummy budget amounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ puts "🌱 Seeding spices..."
     )
   end
 
-10.times do
-    Expense.create(
-        name_of_expense: Faker::Commerce.product_name,
-        amount: Faker::Number.decimal(l_digits: 3, r_digits: 2)
-    )
-end
+# 10.times do
+#     Expense.create(
+#         name_of_expense: Faker::Commerce.product_name,
+#         amount: Faker::Number.decimal(l_digits: 3, r_digits: 2)
+#     )
+# end
 puts "✅ Done seeding!"
